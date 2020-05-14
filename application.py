3507d2a3ab86c59fd0817545289927332956232e
Aishwary Kendle <31218209+aish6999@users.noreply.github.com>
2020-05-14 19:12:32 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h3>Subdomain Takeover by <a href="https://twitter.com/saurabhsanmane2">Saurabh Sanmane</a> & <a href="https://www.linkedin.com/in/aishwarya-kendle/" >Aishwarya Kendle"</a>
+    return "<h3>Subdomain Takeover by <a href='https://twitter.com/saurabhsanmane2'>Saurabh Sanmane</a> & <a href='https://www.linkedin.com/in/aishwarya-kendle/' >Aishwarya Kendle"</a>"
