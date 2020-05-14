613c46b0a806311755717ee8854aec543a2f7b96
Aishwary Kendle <31218209+aish6999@users.noreply.github.com>
2020-05-14 18:52:50 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/poc")
+@app.route("/")
 def hello():
     return "<h3>Subdomain Takeover by <a href="https://twitter.com/saurabhsanmane2">Saurabh Sanmane</a> & <a href="https://www.linkedin.com/in/aishwarya-kendle/" >Aishwarya Kendle"</a>
